ccac4fdd5f28103f037d50111bb21513e9a6fa6f
Sumukh Adiraju <sadiraju06@gmail.com>
2026-07-18 14:11:22 +0100

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -16,11 +16,12 @@ class Solution:
         z = np.dot(x, w) + b 
         y_pred = 1/(1 + np.exp(-z))
 
-        error = y_pred - y_true
-        sigmoid_deriv = (1 - y_pred) * (y_pred)
-        delta = error * sigmoid_deriv
+        error = y_pred - y_true # dL/dy (error signal)
+        sigmoid_deriv = (1 - y_pred) * (y_pred) # dy_pred/dz (Signoid derivative)
+        # dz/dw = x (the input)
+        delta = error * sigmoid_deriv # The delta is the product of the first 2, and tells us "how wrong we are, scaled by how sensitive the ctivation is at this operating point."
 
-        dL_dw = np.round(delta * x, 5)
+        dL_dw = np.round(delta * x, 5) # We use these derivaties to calculate the backpropagation!
         dL_db = round(float(delta), 5)
         return (dL_dw, dL_db)
 
@@ -29,6 +30,7 @@ class Solution:
     Notes:
     Backpropagation is the same idea as gradient descent applied to a neuron: figure out how much each weight contributed to the error, then nudege it in hte direction that reduces the loss. 
     It's just the chain rule, applied one link at a time form the output back to each weight. 
-
-
+    The delta term is the core building block. In deeper networks, deltas propagate backward through layers. 
+    The sigmoud derivative peaks at 0.25 when Y = 0.5 and approaches 0 at the extremes, which is why deep sigmoud networks suffer from vanihsing gradients.
+    The main function to remember here is dL/dw = dL/dy_pred * dy_pred/dz * dz/dw 
     """
