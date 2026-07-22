c3d6c903d5c089905f96a5f8c8b6aa2c254a7337
Sumukh Adiraju <sadiraju06@gmail.com>
2026-07-22 15:55:30 +0100

    Implement Training loop (Training 1/4)

---
+++ foundations/training_loop.py
@@ -0,0 +1,38 @@
+import numpy as np
+from numpy.typing import NDArray
+from typing import Tuple
+
+
+class Solution:
+    def train(self, X: NDArray[np.float64], y: NDArray[np.float64], epochs: int, lr: float) -> Tuple[NDArray[np.float64], float]:
+        # X: (n_samples, n_features)
+        # y: (n_samples,) targets
+        # epochs: number of training iterations
+        # lr: learning rate
+        #
+        # Model: y_hat = X @ w + b
+        # Loss: MSE = (1/n) * sum((y_hat - y)^2)
+        # Initialize w = zeros, b = 0
+        # return (np.round(w, 5), round(b, 5))
+        # Given training data x and targets y, perform gradient descent for a specified number of epochs.
+        n = X.shape[0]
+        w = np.zeros(X.shape[1])
+        b = 0.0
+
+        for _ in range(epochs): # We will iterate this many times
+            y_hat = np.dot(X, w) + b
+
+            error = (y_hat - y) 
+
+            w = w - lr * ((2/n) * (X.T @ error))
+            b = b - lr * ((2/n) * np.sum(error))
+
+        return (np.round(w, 5), round(float(b), 5))
+
+
+
+
+    """
+    Notes:
+    Training time! Everything is useless unless the model actually learns from the data. 
+    """
